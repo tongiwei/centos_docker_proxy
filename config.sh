f2747d755e93597923a0637c8e74c3cd6614e099
Yitong <weiyitong1989@gmail.com>
2024-07-01 17:17:26 +0800

    Update config.sh

--- config.sh
+++ config.sh
@@ -12,5 +12,5 @@ curl -O https://raw.githubusercontent.com/jinwyp/one_click_script/master/install
 
 docker run --name ocserv --privileged -p 443:443 -p 443:443/udp -d dockerepos/ocserv
 
-# install 1panel
-docker run -dt --name 1panel --restart always --network host -v /var/run/docker.sock:/var/run/docker.sock -v /var/lib/docker:/var/lib/docker -v /opt:/opt -e TZ=Asia/Shanghai xeath/1panel-in-docker:latest
+# install baota
+docker run -itd --name baota --network=host --privileged=true --restart=unless-stopped -v ~/www/wwwroot:/www/wwwroot -v ~/www/vhost:/www/server/panel/vhost kissgun/baota:latest
